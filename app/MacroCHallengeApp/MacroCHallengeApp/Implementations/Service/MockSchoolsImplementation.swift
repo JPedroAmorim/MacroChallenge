116c6049252a58bf7895d80f19c5b36495f08e65
Andre Papoti <andrepapoti@gmail.com>
2020-10-07 14:38:58 -0300

    eita

--- app/MacroCHallengeApp/MacroCHallengeApp/Implementations/Service/MockSchoolsImplementation.swift
+++ app/MacroCHallengeApp/MacroCHallengeApp/Implementations/Service/MockSchoolsImplementation.swift
@@ -23,17 +23,21 @@ class MockSchoolsImplementation: SchoolsProtocol {
      */
     
     private func hardCodedSchools() -> [School] {
+        guard let imgCti1 = UIImage(named: "CTI2020questionImage01") else {return []}
+        guard let imgCotuca1 = UIImage(named: "COTUCA2019questionImage04") else {return []}
+        guard let imgCotuca2 = UIImage(named: "COTUCA2019questionImage14") else {return []}
+
         let CtiFirstDummyQuestion = Question(number: "1",
                                           text: "O efeito de humor da tira ocorre com a interpretação equivo- cada que o segundo personagem faz do termo",
                                           initialText: "Leia a tira.",
-                                          image: UIImage(named: "CTI2020questionImage01"),
+                                          images: [imgCti1],
                                           subtitle: "(Mort Walker, “Recruta Zero”. Em: https://cultura.estadao.com.br/quadrinhos)",
                                           options: ["a":"sargento.", "b":"exposição.", "c":"ele.", "d":"venceu."],
                                           answer: "c")
         let CtiSecondDummyQuestion = Question(number: "16",
                                            text: "João ganhou R$ 300,00 de aniversário e utilizou esse dinhei- ro para comprar uma camiseta, uma calça e uma bermuda. A camiseta custou desse valor mais R$ 10,00, e a calça custou desse valor mais R$ 20,00. Com o valor restante foi possível comprar a bermuda e ainda sobraram R$ 45,00. O valor da bermuda foi",
                                            initialText: nil,
-                                           image: nil,
+                                           images: nil,
                                            subtitle: nil,
                                            options: ["a":"R$ 60,00.",
                                                      "b":"R$ 75,00.",
@@ -43,7 +47,7 @@ class MockSchoolsImplementation: SchoolsProtocol {
         let CtiThirdDummyQuestion = Question(number: "18",
                                           text: "Andrea colocou 3 músicas em seu pen drive. A primeira música tinha duração de 3 minutos e 48 segundos, e a segunda músi- ca tinha duração de 2 minutos e 52 segundos. Sabendo que a duração das 3 músicas juntas era de 9 minutos e 15 segundos, então, a duração da terceira música era de",
                                           initialText: nil,
-                                          image: nil,
+                                          images: nil,
                                           subtitle: nil,
                                           options: ["a":"2 minutos e 35 segundos.",
                                                     "b":"2 minutos e 45 segundos.",
@@ -54,7 +58,7 @@ class MockSchoolsImplementation: SchoolsProtocol {
         let CotucaFirstDummyQuestion = Question(number: "4",
                                           text: "Sobre a tirinha de Laerte a seguir e a letra da música “Triste, louca ou má”, é possível afirmar que:",
                                           initialText: "Leia a tira.",
-                                          image: UIImage(named: "COTUCA2019questionImage04"),
+                                          images: [imgCotuca1],
                                           subtitle: "http://oplanetabomba.blogspot.com/2015/10/o-fundamentalismo-e-tentativa-de-moldar.html. Acesso em: 29/07/2018.",
                                           options: ["a":"ambos os textos possuem uma relação temática ao problematizarem a ideia de normatização de certos comportamentos sociais.",
                                                     "b":"ambos os textos possuem uma relação temática ao afirmarem a necessidade de superação do conceito tradicional de família.",
@@ -65,7 +69,7 @@ class MockSchoolsImplementation: SchoolsProtocol {
         let CotucaSecondDummyQuestion = Question(number: "14",
                                            text: "Desse modo, um cliente que almoçar dez vezes e ganhar o almoço por conta do restaurante ganhará um desconto cujo valor, aproximadamente, equivale a:",
                                            initialText: "Um restaurante faz uma promoção, com a seguinte regra, a seus clientes fidelizados:",
-                                           image: UIImage(named: "COTUCA2019questionImage14"),
+                                           images: [imgCotuca2],
                                            subtitle: nil,
                                            options: ["a":"8,9%.",
                                                      "b":"9,1%.",
@@ -75,7 +79,7 @@ class MockSchoolsImplementation: SchoolsProtocol {
         let CotucaThirdDummyQuestion = Question(number: "16",
                                           text: "Uma fruta in natura possui 80% de sua massa composta de água e, se for desidratada, a água se reduz a 10% da massa após esse processo. Qual é a massa (em gramas) dessa fruta in natura que corresponderia a uma porção de 100 g dessa mesma fruta em sua forma desidratada?",
                                           initialText: nil,
-                                          image: nil,
+                                          images: nil,
                                           subtitle: nil,
                                           options: ["a":"900g",
                                                     "b":"890g",

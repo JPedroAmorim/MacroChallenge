791bf5826bb31a6fbe2383fc7b58a39298a20728
Makoto <rmt.makoto@gmail.com>
2020-10-15 12:17:36 -0300

    Navigation color changed

--- app/MacroCHallengeApp/MacroCHallengeApp/App and Scene Delegates/AppDelegate.swift
+++ app/MacroCHallengeApp/MacroCHallengeApp/App and Scene Delegates/AppDelegate.swift
@@ -12,12 +12,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-
-		UINavigationBar.appearance().barTintColor = UIColor(named: "NavigationBarColor")
-		UINavigationBar.appearance().tintColor = .white
-		UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-		UINavigationBar.appearance().isTranslucent = false
 		
+        UINavigationBar.appearance().tintColor = UIColor(named: "NavigationBarColor")
+        
         return true
     }
 

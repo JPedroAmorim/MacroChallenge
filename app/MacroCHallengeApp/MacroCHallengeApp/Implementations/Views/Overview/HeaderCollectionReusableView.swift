16f1f7f09ba469e1b1caa6ed088dc84121263f63
Felipe Semissatto <felipesemi@gmail.com>
2020-10-16 13:37:39 -0300

    Implementando Header

---
+++ app/MacroCHallengeApp/MacroCHallengeApp/Implementations/Views/Overview/HeaderCollectionReusableView.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderCollectionReusableView.swift
+//  MacroCHallengeApp
+//
+//  Created by Felipe Semissatto on 16/10/20.
+//
+
+import UIKit
+
+class HeaderCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}

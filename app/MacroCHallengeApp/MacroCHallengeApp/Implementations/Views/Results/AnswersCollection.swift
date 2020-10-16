2bc051ffcc60095812006744989f5ba7a9ef6bd8
Joao Victor Flores <j199818@dac.unicamp.br>
2020-10-16 10:01:44 -0300

    filesCreated

---
+++ app/MacroCHallengeApp/MacroCHallengeApp/Implementations/Views/Results/AnswersCollection.swift
@@ -0,0 +1,8 @@
+//
+//  AnswersCollection.swift
+//  MacroCHallengeApp
+//
+//  Created by Joao Flores on 16/10/20.
+//
+
+import Foundation
---
+++ app/MacroCHallengeApp/MacroCHallengeApp/Implementations/Views/Results/AnswersCollectionCell.swift
@@ -0,0 +1,8 @@
+//
+//  AnswersCollectionCell.swift
+//  MacroCHallengeApp
+//
+//  Created by Joao Flores on 16/10/20.
+//
+
+import Foundation

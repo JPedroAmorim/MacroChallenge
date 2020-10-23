62fb841377e9595a687c76a81006570fae64e116
Felipe Semissatto <felipesemi@gmail.com>
2020-10-23 09:19:39 -0300

    arrumando as chaves dos mocks

--- app/MacroCHallengeApp/MacroCHallengeApp/Implementations/Service/MockSchoolsImplementation.swift
+++ app/MacroCHallengeApp/MacroCHallengeApp/Implementations/Service/MockSchoolsImplementation.swift
@@ -777,7 +777,7 @@ class MockSchoolsImplementation: SchoolsProtocol {
                                                   Etec2019ThirdDummyQuestion])]
         
         
-        let topics = ["matematica":["Operações com números inteiros e fracionários",
+        let topics = ["Matemática":["Operações com números inteiros e fracionários",
                                     "Sistema de medidas usuais",
                                     "Números racionais relativos",
                                     "Regra de três simples",
@@ -792,7 +792,7 @@ class MockSchoolsImplementation: SchoolsProtocol {
                                     "Fatoração",
                                     "Simplificação de expressões algébricas.",
                                     "Produtos notáveis"],
-                      "portugues":["Interpretação de texto",
+                      "Português":["Interpretação de texto",
                                    "Sinônimos e antônimos",
                                    "sinônimos e antônimos",
                                    "acentuação gráfica",
@@ -807,7 +807,7 @@ class MockSchoolsImplementation: SchoolsProtocol {
                                    "sintaxe: tipos de sujeito e tipos de predicado",
                                    "figuras de linguagem",
                                    "linguagens e variações linguísticas"],
-                      "cienciasNaturais":["Drogas: categorias, efeitos, problemas sociais e de  saúde decorrentes do uso",
+                      "Ciências Naturais":["Drogas: categorias, efeitos, problemas sociais e de  saúde decorrentes do uso",
                                           "métodos contraceptivos humanos",
                                           "sistema imunológico, antígenos e anticorpos, vacinas e soros",
                                           "antibióticos",
@@ -841,7 +841,7 @@ class MockSchoolsImplementation: SchoolsProtocol {
                                           "eletricidade básica (átomo, corpos condutores e isolantes, eletrizados e neutros, montagem de circuitos elétricos simples)",
                                           "magnetismo (ímãs e campo magnético)",
                                           "ondas (classificações)"],
-                      "cienciasHumanas":["Lugar, paisagem e espaço geográfico",
+                      "Ciências Humanas":["Lugar, paisagem e espaço geográfico",
                                          "noções básicas de cartografia e geomorfologia",
                                          "paisagens naturais e suas relações biogeográficas (os grandes biomas terrestres)",
                                          "aspectos naturais e socioeconômicos do Brasil",
@@ -868,13 +868,13 @@ class MockSchoolsImplementation: SchoolsProtocol {
                                          "a crise do Socialismo Real",
                                          "o Neoliberalismo",
                                          "Liberalismo econômico"]]
-        let numberOfQuestionPerTopic = ["matematica": 15,
-                                        "portugues": 15,
-                                        "cienciasNaturais": 15,
-                                        "cienciasHumanas": 5]
-        let essay = ["redacaoTema":"considera-se se o texto do candidato atende ao tema proposto. A fuga completa ao tema proposto é motivo suficiente para que a redação não seja corrigida em qualquer outro de seus aspectos, recebendo nota 0 (zero) total.",
-        "redacaoEstrutura":"consideram-se aqui, conjuntamente, os aspectos referentes ao gênero/tipo de texto proposto e à coerência das ideias. A fuga completa ao gênero/tipo de texto é motivo suficiente para que a redação não seja corrigida em qualquer outro de seus aspectos, recebendo nota 0 (zero) total. Avalia-se aqui como o candidato sustenta seu ponto de vista em termos argumentativos e como essa argumentação está organizada, considerando-se a macroestrutura do texto (introdução, desenvolvimento e conclusão). Será considerada aspecto negativo a referência direta à situação imediata de produção textual (ex.: como afirma o autor do primeiro texto/da coletânea/do texto I; como solicitado nesta prova/proposta de redação). Na coerência, será observada, além da pertinência dos argumentos mobilizados para a defesa do ponto de vista, a capacidade do candidato de encadear as ideias de forma lógica e coerente (progressão textual). Serão consideradas aspectos negativos a presença de contradições entre as ideias, a falta de partes da macroestrutura do texto, a falta de desenvolvimento das ideias e a presença de conclusões não decorrentes do que foi previamente exposto.",
-        "redacaoExpressao":"consideram-se nesse item os aspectos referentes à coesão textual e ao domínio da norma-padrão da língua portuguesa. Na coesão, avalia-se a utilização dos recursos coesivos da língua (anáforas, catáforas, substituições, conjunções etc.), de modo a tornar a relação entre frases e períodos e entre os parágrafos do texto mais clara e precisa. Serão considerados aspectos negativos as quebras entre frases ou parágrafos e o emprego inadequado de recursos coesivos. Na modalidade, serão examinados os aspectos gramaticais, tais como ortografia, acentuação, pontuação, regência, concordância (verbal e nominal) etc., bem como a escolha lexical (precisão vocabular) e o grau de formalidade/informalidade expressa em palavras e expressões."]
+        let numberOfQuestionPerTopic = ["Matemática": 15,
+                                        "Português": 15,
+                                        "Ciências Naturais": 15,
+                                        "Ciências Humanas": 5]
+        let essay = ["Tema":"considera-se se o texto do candidato atende ao tema proposto. A fuga completa ao tema proposto é motivo suficiente para que a redação não seja corrigida em qualquer outro de seus aspectos, recebendo nota 0 (zero) total.",
+        "Estrutura":"consideram-se aqui, conjuntamente, os aspectos referentes ao gênero/tipo de texto proposto e à coerência das ideias. A fuga completa ao gênero/tipo de texto é motivo suficiente para que a redação não seja corrigida em qualquer outro de seus aspectos, recebendo nota 0 (zero) total. Avalia-se aqui como o candidato sustenta seu ponto de vista em termos argumentativos e como essa argumentação está organizada, considerando-se a macroestrutura do texto (introdução, desenvolvimento e conclusão). Será considerada aspecto negativo a referência direta à situação imediata de produção textual (ex.: como afirma o autor do primeiro texto/da coletânea/do texto I; como solicitado nesta prova/proposta de redação). Na coerência, será observada, além da pertinência dos argumentos mobilizados para a defesa do ponto de vista, a capacidade do candidato de encadear as ideias de forma lógica e coerente (progressão textual). Serão consideradas aspectos negativos a presença de contradições entre as ideias, a falta de partes da macroestrutura do texto, a falta de desenvolvimento das ideias e a presença de conclusões não decorrentes do que foi previamente exposto.",
+        "Expressão":"consideram-se nesse item os aspectos referentes à coesão textual e ao domínio da norma-padrão da língua portuguesa. Na coesão, avalia-se a utilização dos recursos coesivos da língua (anáforas, catáforas, substituições, conjunções etc.), de modo a tornar a relação entre frases e períodos e entre os parágrafos do texto mais clara e precisa. Serão considerados aspectos negativos as quebras entre frases ou parágrafos e o emprego inadequado de recursos coesivos. Na modalidade, serão examinados os aspectos gramaticais, tais como ortografia, acentuação, pontuação, regência, concordância (verbal e nominal) etc., bem como a escolha lexical (precisão vocabular) e o grau de formalidade/informalidade expressa em palavras e expressões."]
         
         let notice = Notice(topics: topics,
                             numberOfQuestionsPerTopic: numberOfQuestionPerTopic,

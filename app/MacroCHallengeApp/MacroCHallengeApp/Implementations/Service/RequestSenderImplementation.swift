b14588db7f07c625245de0488bbdb7c6a961690c
João Pedro <joaopdamorim@gmail.com>
2020-10-26 15:10:46 -0300

    alamofire pod added

---
+++ app/MacroCHallengeApp/MacroCHallengeApp/Implementations/Service/RequestSenderImplementation.swift
@@ -0,0 +1,17 @@
+//
+//  RequestSenderImplementation.swift
+//  MacroCHallengeApp
+//
+//  Created by João Pedro de Amorim on 26/10/20.
+//
+
+import Foundation
+import Alamofire
+
+class RequestSenderImplementation: RequestSenderProtocol {
+    func getQuestionsForTestRequest(testName: String, testYear: String, completion: ([Question]) -> Void, onFailure: (String) -> Void) {
+        <#code#>
+    }
+    
+    private func sendRequest
+}

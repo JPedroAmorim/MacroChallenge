2f3ba3b256a948b099807e2a24890e21e69cda06
Felipe Semissatto <felipesemi@gmail.com>
2020-11-11 16:25:46 -0300

    AddNewUser chama a tela após fazer o login (histórico incompleto))

--- app/MacroCHallengeApp/MacroCHallengeApp/Implementations/Controllers/LoginViewControllerImplementation.swift
+++ app/MacroCHallengeApp/MacroCHallengeApp/Implementations/Controllers/LoginViewControllerImplementation.swift
@@ -16,7 +16,7 @@ class LoginViewControllerImplementation: UIViewController,  LoginViewControllerP
     // MARK: - Lifecycle methods
     required init(message: String) {
         super.init(nibName: nil, bundle: nil)
-        self.view = LoginViewImplementation(message: "Para ver \(message), faça o login com sua conta Apple :)", controller: self)
+        self.view = LoginViewImplementation(message: "Para ver \(message), faça o login com sua conta Apple ;)", controller: self)
         let backButton = UIBarButtonItem(title: "", style: .plain, target: navigationController, action: nil)
         navigationItem.leftBarButtonItem = backButton
         navigationItem.title = message
@@ -26,13 +26,43 @@ class LoginViewControllerImplementation: UIViewController,  LoginViewControllerP
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Private Methods
+    /**
+     
+     Método responsável por chamar a tela logo após do usuário realizar seu primeiro login.
+     
+     - parameter item: número que dá o contexto da tela (1, se está na tela de métricas; 2, se está na tela de histórico).
+     
+     */
+    private func pushController(_ item: Int) {
+        switch item {
+        case 1: // Métricas Gerais
+            let controller = MetricsViewControllerImplementation()
+            if let navController = self.navigationController {
+                navController.pushViewController(controller, animated: false)
+            }
+            break
+        case 2: // Histórico
+//            let controller = HistoricViewControllerImplementation(coder: <#T##NSCoder#>)
+//            if let navController = self.navigationController {
+//                navController.pushViewController(controller, animated: false)
+//            }
+            break
+        default:
+            print("Another tab was selected")
+        }
+    }
+    
     // MARK: - LoginViewControllerProtocol methods
     func addNewUser(_ userIdentifier: String) {
         // Adicionando usuário no UserDefaults
         UserDefaults.standard.set(userIdentifier, forKey: "User")
         
         
-        
+        self.navigationController?.popViewController(animated: true)
+        if let item = tabBarController?.selectedIndex {
+            pushController(item)
+        }
     }
     
     func checkUserExistence(_ userIdentifier: String) {

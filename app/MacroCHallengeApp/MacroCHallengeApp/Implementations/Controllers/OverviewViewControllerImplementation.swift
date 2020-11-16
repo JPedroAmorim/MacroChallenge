321b4f3dc470929597d6b7d1a2db59376b46ed42
João Pedro <joaopdamorim@gmail.com>
2020-11-16 14:29:14 -0300

    overview vc set timer to 4 hours

--- app/MacroCHallengeApp/MacroCHallengeApp/Implementations/Controllers/OverviewViewControllerImplementation.swift
+++ app/MacroCHallengeApp/MacroCHallengeApp/Implementations/Controllers/OverviewViewControllerImplementation.swift
@@ -10,7 +10,7 @@ import UIKit
 class OverviewViewControllerImplementation: UIViewController, OverviewViewControllerProtocol {
     // MARK: - Constants
     // TODO: Ver como esse controller de fato obterá essa informação
-    private let testDurationInSeconds = 10 * 60 // Valor de teste: Dez minutos
+    private let testDurationInSeconds = 4 * 60 * 60 // Quatro horas
     
     // MARK: - Dependencies
     var myView: OverviewViewProtocol?
